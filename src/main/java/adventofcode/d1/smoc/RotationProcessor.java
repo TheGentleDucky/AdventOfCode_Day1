652796f7ca356ca9f1a6b5bc15ca8f2142eccfa9
TheGentleDucky <owenscruzsean@gmail.com>
2025-12-07 11:42:53 +0000

    Day 1 Challenge-Part 2 completed

--- src/main/java/adventofcode/d1/smoc/RotationProcessor.java
+++ src/main/java/adventofcode/d1/smoc/RotationProcessor.java
@@ -7,16 +7,26 @@ public class RotationProcessor {
     public int countZeros(Dial dial, List<Rotation> rotations) {
         int zeroCount = 0;
 
-        for (Rotation rotation: rotations) {
-            int newPosition = switch (rotation.getDirection()) {
-                case LEFT -> dial.turnLeft(rotation.getClicks());
-                case RIGHT -> dial.turnRight(rotation.getClicks());
-            };
 
-            if (newPosition == 0) {
-                zeroCount++;
+        for (Rotation rotation : rotations) {
+            int clicks = rotation.getClicks();
+
+            for (int i = 0; i < clicks; i++) {
+                int newPosition = rotateOnce(dial, rotation.getDirection());
+
+                if (newPosition == 0) {
+                    zeroCount++;
+                }
             }
         }
+
         return zeroCount;
+    }
+
+    private int rotateOnce(Dial dial, Rotation.Direction direction) {
+        return switch (direction) {
+            case LEFT -> dial.turnLeft(1);
+            case RIGHT -> dial.turnRight(1);
+        };
     }
 }
